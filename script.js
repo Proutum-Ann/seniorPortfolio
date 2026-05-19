e2eddcc235fa4993d512b91f69e349665558eab7
Proutum-Ann <liangproutum@gmail.com>
2026-05-18 21:05:16 -0400

    data now loads stuff

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const vue_app = Vue.createApp({
       //   the variable: movies
       created () {
             fetch('sophomore.json').then(response => response.json()).then(json => {
-                  this.soph = json
+                  this.soph = json.projects || []
             })
             fetch('junior.json').then(response => response.json()).then(json => {
-                  this.junior = json
+                  this.junior = json.projects || []
             })
             fetch('senior.json').then(response => response.json()).then(json => {
-                  this.senior = json
+                  this.senior = json.projects || []
             })
             fetch('extras.json').then(response => response.json()).then(json => {
-                  this.extras = json.projects || []
+                  this.extras = json.files || []
             })
       },
       data() {
@@ -20,11 +20,51 @@ const vue_app = Vue.createApp({
             soph: [],
             junior: [],
             senior: [],
-            extras: []
+            extras: [],
+
+            activeModal: null,
+            activeProject: null,
+            activeProjects: [],
+            activeAbout: null,
+            activeWorks: [],
         }
     },
     methods: {
-      
+      async openMainModal(apps) {
+            const response = await fetch(apps.destination);
+            const data = await response.json();
+
+            this.activeModal = apps;
+            this.activeProject = null;
+            this.activeProjects = [];
+            this.activeAbout = null;
+            this.activeWorks = [];
+
+            if (apps.type === "year") {
+                  this.activeProjects = data.projects || [];
+            }
+
+            if (apps.type === "about") {
+                  this.activeAbout = data;
+            }
+
+            if (apps.type === "works") {
+                  this.activeWorks = data.works || [];
+            }
+            },
+
+            openProject(project) {
+            this.activeProject = project;
+            },
+
+            closeMainModal() {
+            this.activeModal = null;
+            this.activeProject = null;
+            },
+
+            closeProjectModal() {
+            this.activeProject = null;
+      }
     }
 })
 
